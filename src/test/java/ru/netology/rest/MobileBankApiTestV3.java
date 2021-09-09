23edf85dcae42b9614258f4ca5be370e9f1b388d
Maksim Shalaev <maksimshalaev11@gmail.com>
2021-09-09 21:32:00 +0300

    test#3 break

--- src/test/java/ru/netology/rest/MobileBankApiTestV3.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -20,7 +20,7 @@ public class MobileBankApiTestV3 {
                 .then()
                 .statusCode(200)
                 .contentType(ContentType.JSON)
-                .body("data", equalTo("some data"))
+                .body("data", equalTo("some ddta"))
         ;
     }
 }
